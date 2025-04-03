a247000da54ce8c5db0881c1bded213ced52d6db
praburaj005 <46099429+praburaj005@users.noreply.github.com>
2025-04-03 15:31:34 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
