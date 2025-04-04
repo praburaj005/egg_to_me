5e2bc659ed4e59c8e06f3938aa11a3aac2e1f071
praburaj005 <46099429+praburaj005@users.noreply.github.com>
2025-04-04 17:59:58 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(=7gu-zjvsm5rk1ur8q72e9!87ww4qn@0ut5w@&eq)hegi*t$*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [''zero.onrender.com'']
+ALLOWED_HOSTS = ['zero.onrender.com']
 
 
 # Application definition
